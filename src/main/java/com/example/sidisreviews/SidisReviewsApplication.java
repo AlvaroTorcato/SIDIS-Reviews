76979edeb12aa27c84a155e9d0b30c3e506d5728
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-10-19 09:42:10 +0100

    Initial commit

---
+++ src/main/java/com/example/sidisreviews/SidisReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sidisreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidisReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidisReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sidisreviews/SidisReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidisreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidisReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
